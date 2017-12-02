8815dd1d1248e24b9fc16d04375fa08b57d617c1
Ben <chippolot@gmail.com>
2017-12-02 14:57:54 -0800

    Removing old files

--- 2017/2_2.hs
+++
@@ -1,9 +0,0 @@
-checksumLine :: String -> Int
-checksumLine line = 
-    let nums = map (read::String->Int) (words line)
-        in head [x `div` y | x <- nums, y <- nums, x `rem` y == 0, x /= y]
-
-main = do
-    input <- readFile "input.txt"
-    let checksum = sum $ map checksumLine $ lines input
-    print checksum
