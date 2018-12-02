fbceda036e5ab68b41d94e14d018a49f41e95dbf
Ben <chippolot@gmail.com>
2018-12-01 23:48:23 -0800

    Cleaned up day 2p1

--- 2018/02/1_1.rb
+++ 2018/02/1_1.rb
@@ -4,9 +4,9 @@ twos = 0
 threes = 0
 
 lines.each do |line|
-    counts = {}
+    counts = Hash.new{0}
     line.each_char do |char|
-        counts[char] = (counts[char] or 0) + 1
+        counts[char] += 1
     end
     twos += 1 if counts.value? 2
     threes += 1 if counts.value? 3
