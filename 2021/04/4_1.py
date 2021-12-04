f85183e75a25259317a26b98a3209cf49ddb1616
Ben Smith <chippolot+github@gmail.com>
2021-12-03 21:48:59 -0800

    Day 4 2021

---
+++ 2021/04/4_1.py
@@ -0,0 +1,34 @@
+def call_numbers(boards, num):
+    for board in boards:
+        board[0].discard(num)
+        for winner in board[1]:
+            winner.discard(num)
+            if len(winner) == 0:
+                return board
+    return None
+
+def board_row_winner(board_def, row):
+    return set(board_def[row])
+
+def board_col_winner(board_def, col):
+    col_nums = set()
+    for i in range(0,5):
+        col_nums.add(board_def[i][col])
+    return col_nums
+
+def parse_board(data):
+    board_def = [list(map(int,row.split())) for row in data.split('\n')]
+    all_nums = set().union(*board_def)
+    winners = [board_row_winner(board_def, row) for row in range(0,5)] + [board_col_winner(board_def, col) for col in range(0,5)]
+    return [all_nums, winners]
+
+groups = open('input.txt', 'r').read().split('\n\n')
+calls = list(map(int,groups[0].split(',')))
+
+boards = [parse_board(x) for x in groups[1:]]
+for num in calls:
+    winner = call_numbers(boards, num)
+    if winner:
+        unmarked_sum = sum(winner[0])
+        print(unmarked_sum * num)
+        break
